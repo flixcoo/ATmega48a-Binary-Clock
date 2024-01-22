93316108fef09068c3e61f2865cd2fd5ac7445bb
Felix Kirchner <felix.kirchner@stud.htwk-leipzig.de>
2024-01-22 20:54:37 +0000

    Add led_test.c

---
+++ led_test.c
@@ -0,0 +1,63 @@
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+void main() {
+	DDRC = (1<<5);
+	while (1) {
+		//C0
+		PORTC|=(1<<0);
+		_delay_ms(200);
+		PORTC&=~(1<<0);
+
+		//C1
+		PORTC|=(1<<1);
+		_delay_ms(200);
+		PORTC&=~(1<<1);
+		
+		//C2
+		PORTC|=(1<<2);
+		_delay_ms(200);
+		PORTC&=~(1<<2);
+
+		//C3
+		PORTC|=(1<<3);
+		_delay_ms(200);
+		PORTC&=~(1<<3);
+
+		//C4
+		PORTC|=(1<<4);
+		_delay_ms(200);
+		PORTC&=~(1<<4);
+		
+		//C5
+		PORTC|=(1<<5);
+		_delay_ms(200);
+		PORTC&=~(1<<5);
+		
+		//D3
+		PORTD|=(1<<3);
+		_delay_ms(200);
+		PORTD&=~(1<<3);
+		
+		//D4
+		PORTD|=(1<<4);
+		_delay_ms(200);
+		PORTD&=~(1<<4);
+		
+		//D5
+		PORTD|=(1<<5);
+		_delay_ms(200);
+		PORTD&=~(1<<5);
+		
+		//D6
+		PORTD|=(1<<6);
+		_delay_ms(200);
+		PORTD&=~(1<<6);
+		
+		//D7
+		PORTD|=(1<<7);
+		_delay_ms(200);
+		PORTD&=~(1<<7);
+		}
+}
