0c61e4bba4d0747a0d04ac709f8ef5261f4e68dc
Felix Kirchner <felix.kirchner@stud.htwk-leipzig.de>
2024-01-23 16:34:33 +0100

    formatting & naming

--- led_test.c
+++ led_test.c
@@ -11,43 +11,36 @@ void main() {
 		DDRD = (0xF8);//11111000
 
 		//C0
-	
 		PORTC|=(1<<0);
 		_delay_ms(time);
 		PORTC&=~(1<<0);
 
 		//C1
-	
 		PORTC|=(1<<1);
 		_delay_ms(time);
 		PORTC&=~(1<<1);
 		
 		//C2
-	
 		PORTC|=(1<<2);
 		_delay_ms(time);
 		PORTC&=~(1<<2);
 
 		//C3
-	
 		PORTC|=(1<<3);
 		_delay_ms(time);
 		PORTC&=~(1<<3);
 
 		//C4
-	
 		PORTC|=(1<<4);
 		_delay_ms(time);
 		PORTC&=~(1<<4);
 		
 		//C5
-	
 		PORTC|=(1<<5);
 		_delay_ms(time);
 		PORTC&=~(1<<5);
 		
 		//D3
-	
 		PORTD|=(1<<3);
 		_delay_ms(time);
 		PORTD&=~(1<<3);
@@ -73,16 +66,14 @@ void main() {
 		PORTD&=~(1<<7);
 		
 		_delay_ms(500);
-		alleAn();
+        allLedsOn();
 		_delay_ms(500);
-		alleAus();
+        allLedsOff();
 		_delay_ms(500);
-
-
 		}
 }
 
-void alleAn(){
+void allLedsOn(){
 	PORTC|=(1<<0);
 	PORTC|=(1<<1);
 	PORTC|=(1<<2);
@@ -96,7 +87,7 @@ void alleAn(){
 	PORTD|=(1<<7);
 }
 
-void alleAus(){
+void allLedsOff(){
 	PORTC&=~(1<<0);
 	PORTC&=~(1<<1);
 	PORTC&=~(1<<2);
